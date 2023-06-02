25e9c56058aa561bdddd33630fa491da9364fa03
Lucas <lucfg15@gmail.com>
2023-06-02 15:41:22 -0300

    first commit

---
+++ contams/src/main/java/br/com/bantads/contams/ContamsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bantads.contams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContamsApplication.class, args);
+	}
+
+}
